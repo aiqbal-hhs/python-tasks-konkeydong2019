a075688d6e7d6d7074d4e2010bdcbe4bd8f486c5
konkeydong2019 <51100351+konkeydong2019@users.noreply.github.com>
2020-07-30 14:39:43 +1200

    Add files via upload

--- wageiesg.py
+++ wageiesg.py
@@ -1,8 +1,34 @@
-hours = float(input("how many hours have you worked today?\n"))
-if hours >= 24:
-       print("no, you didn't")
+i = 0
+
+while i == 0:
+       days = int(input("how many days do you work per week?\n"))
+
+       if days <= 0:
+              print("get a job")
+              break
+       elif days > 7:
+              print("there are 7 days in a week\n")
+       else:
+              i =1
+
+while i == 1:
+       hours = float(input("how many hours do you work everyday?\n"))
+
+       if hours >= 24:
+              print("no, you didn't")
+       elif hours <= 0:
+              print("You will recieve your pay when you do some work.")
+       else:
+              i = 2
+
+while i == 2:
+       wage = float(input("what is your hourly rate?\n"))
+
+       if wage > 0:
+              i = 3
+
+tha = wage * hours * days
+
+print("Your weekly income is ${}" .format(tha))
+print("and after ${} tax" .format(round(tha * 0.8, 2) ))
        
-elif hours <= 0:
-       print("You will recieve your pay when you do some work.")
-else:
-       print("That is ${} for today." .format(hours * 15))
